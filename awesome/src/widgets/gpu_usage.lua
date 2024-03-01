a9d58403473f4fdbd96e8df78846b8d9c2a74c07
Moogs <saphielle.akiyama@gmail.com>
2024-03-02 00:01:59 +0100

    Add animation on gpu widget hover

--- awesome/src/widgets/gpu_usage.lua
+++ awesome/src/widgets/gpu_usage.lua
@@ -1,12 +1,13 @@
 -- Gpu usage widget
-
 local awful = require("awful")
 local color = require("src.theme.colors")
 local dpi = require("beautiful").xresources.apply_dpi
 local utils = require("src.widgets.utils")
 
+local gpu_widget_bg = color["Green100"]
+
 local gpu_widget = utils.watch_widget{
-    bg = color["Green200"],
+    bg = gpu_widget_bg,
     image_filename = "gpu.svg",
     watch_script_filename = "gpu_usage.sh",
     update_interval = 1,
@@ -26,15 +27,21 @@ local gpu_tooltip = awful.tooltip{
 gpu_widget:connect_signal(
     "mouse::enter",
     function()
+        gpu_widget.bg = color["Green200"]
+
         awful.spawn.easy_async_with_shell(
             [[lspci | grep -i vga | cut -d ' ' -f5- | sed 's/^/- /']],
             function(stdout)
-                gpu_tooltip:set_text("Detected GPUs\n" .. stdout)
+                gpu_tooltip:set_text("Detected GPUs:\n" .. stdout)
             end
         )
     end
 )
-
-
+gpu_widget:connect_signal(
+    "mouse::leave",
+    function()
+        gpu_widget.bg = gpu_widget_bg
+    end
+)
 
 return gpu_widget
